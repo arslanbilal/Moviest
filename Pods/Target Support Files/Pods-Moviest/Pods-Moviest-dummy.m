6c49fd6cc116ee2f45110e937dbc5c59e93ab386
Bilal Arslan <bilal.arslan1993@hotmail.com>
2018-11-14 20:23:33 +0300

    delete pods folder

--- Pods/Target Support Files/Pods-Moviest/Pods-Moviest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Moviest : NSObject
-@end
-@implementation PodsDummy_Pods_Moviest
-@end
--- Pods/Target Support Files/Pods-MoviestTests/Pods-MoviestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoviestTests : NSObject
-@end
-@implementation PodsDummy_Pods_MoviestTests
-@end
