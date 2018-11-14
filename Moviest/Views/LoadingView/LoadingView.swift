38476ada546fc664297f231939e55cb877318a11
Bilal Arslan <bilal.arslan1993@hotmail.com>
2018-11-14 18:35:47 +0300

    update loadingView

--- Moviest/Views/LoadingView/LoadingView.swift
+++ Moviest/Views/LoadingView/LoadingView.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class LoadingView: UIView, NibLoadable, Instantiatable {
 
+    @IBOutlet private weak var loadingView: UIView!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var loadingLabel: UILabel!
+
     override func awakeFromNib() {
-        layer.cornerRadius = 5
+        loadingView.layer.cornerRadius = 5
+        loadingView.backgroundColor = .clear
+        loadingLabel.textColor = .black
+        activityIndicator.color = .black
+
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        loadingView.insertSubview(blurView, at: 0)
+
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: loadingView.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: loadingView.widthAnchor),
+            ])
     }
 
 }
